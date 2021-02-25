8faa63b6897bdf737ef10a003853531374df8a24
Ilya Saenko <s41k4gg@gmail.com>
2021-02-25 20:44:26 +0300

    Class work

---
+++ Laba3/Numerical algorithms.pl
@@ -0,0 +1,23 @@
+max(X,Y,X):-X>Y,!.%max(X,Y,Z):-X>Y,Z is X.
+max(_,Y,Y).% max(X,Y,Y):-X=<Y max(X,Y,Z):-X=<Y,Z is Y.
+%2 TASK
+max(X,Y,U,X):-X>Y,X>U,!.
+max(_,Y,U,Y):-Y>U,!.
+max(_,_,U,U).
+
+max1(X,Y,U,Z):-max(X,Y,C),max(C,U,Z).
+%Rekursiya vverx
+fact(0,1):-!.
+fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
+%Rekursiya vniz
+fact1(N,X,N,X):-!.
+fact1(I,F,N,X):-I1 is I+1,F1 is F*I1,fact1(I1,F1,N,X).
+fact1(N,F):-fact1(0,1,N,F).%Сюда мы спрятали страшный факториал
+%7
+sum(0,0):-!.
+sum(N,S):-N1 is N div 10,sum(N1,S1),S is S1+N mod 10.
+%8 vniz rekyrsiya
+sum1(N,S):-sum1(N,0,S).
+sum1(0,S,S):-!.
+sum1(A,Sum,S):-Am is A mod 10, Ad is A div 10,
+               Sum1 is Sum+Am,sum1(Ad,Sum1,S).
